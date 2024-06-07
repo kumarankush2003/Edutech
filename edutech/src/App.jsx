b24775b3e5490ccfdc26b4fb91170f4b8df04613
kumarankush2003 <ankushkumar642003@gmail.com>
2024-06-07 19:52:29 +0530

    navbar frontend

--- edutech/src/App.jsx
+++ edutech/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
+import Navbar from './navbar/Navbar';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
